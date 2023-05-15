f4c10be941ccfb4a69ce2fbbbc54ed6ed2156835
BECATRUE <becatrueyjh@gmail.com>
2023-05-15 16:35:41 +0900

    Implement frames() in explorer app
    It returns the explorer frame.
    
    See also: #6

--- iquip/explorer.py
+++ iquip/explorer.py
@@ -2,7 +2,7 @@
 App module for showing the experiment list and opening an experiment.
 """
 
-from typing import Optional
+from typing import Optional, Tuple
 
 from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QLabel, QWidget
@@ -23,3 +23,7 @@ class ExplorerApp(BaseApp):
         """Extended."""
         super().__init__(name, parent=parent)
         self.explorerFrame = ExplorerFrame()
+
+    def frames(self) -> Tuple[ExplorerFrame]:
+        """Overridden."""
+        return (self.explorerFrame,)
