1fe2222a96cf9dab9f0b4965084155516d8a1682
BECATRUE <becatrueyjh@gmail.com>
2023-05-15 16:33:58 +0900

    Create explorer app
    Create the explorer app with the explorer frame.
    
    See also: #6

--- iquip/explorer.py
+++ iquip/explorer.py
@@ -7,9 +7,19 @@ from typing import Optional
 from PyQt5.QtCore import QObject
 from PyQt5.QtWidgets import QLabel, QWidget
 
+from qiwis import BaseApp
+
 class ExplorerFrame(QWidget):
     def __init__(self, parent: Optional[QObject] = None):
         """Extended."""
         super().__init__(parent=parent)
         # widgets
         self.testLabel = QLabel("explorer", self)
+
+
+class ExplorerApp(BaseApp):
+    """App for showing the experiment list and opening an experiment."""
+    def __init__(self, name: str, parent: Optional[QObject] = None):
+        """Extended."""
+        super().__init__(name, parent=parent)
+        self.explorerFrame = ExplorerFrame()
