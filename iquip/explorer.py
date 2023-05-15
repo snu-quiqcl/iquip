8f8ab5e5860e8fe4bdfa76c633736c1d6e23579c
BECATRUE <becatrueyjh@gmail.com>
2023-05-15 16:29:44 +0900

    Create explorer frame
    Create the frame temporarily.
    It contains a label for test.
    
    see also: #6

--- iquip/explorer.py
+++ iquip/explorer.py
@@ -1,3 +1,15 @@
 """
 App module for showing the experiment list and opening an experiment.
 """
+
+from typing import Optional
+
+from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QLabel, QWidget
+
+class ExplorerFrame(QWidget):
+    def __init__(self, parent: Optional[QObject] = None):
+        """Extended."""
+        super().__init__(parent=parent)
+        # widgets
+        self.testLabel = QLabel("explorer", self)
