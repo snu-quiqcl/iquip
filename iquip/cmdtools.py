7e9a0310f62bc625f7b06de629db2cddd62ef76f
BECATRUE <becatrueyjh@gmail.com>
2023-05-17 17:38:14 +0900

    Update subprocess.run()
    See also: #11

--- iquip/cmdtools.py
+++ iquip/cmdtools.py
@@ -8,8 +8,8 @@ def run_command(cmd):
     If the command line is completed without any error,
     the printed result would be stored in the 'stdout' field of the return value.
     Otherwise, the printed error would be stored in the 'stderr' field.
-    
+
     Args:
         cmd: The string of the command line to run.
     """
-    return subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return subprocess.run(cmd, capture_output=True, shell=True, check=True, text=True)
