23030ce81d0a9e46d6d07560fa083adfce770978
BECATRUE <becatrueyjh@gmail.com>
2023-05-17 12:08:08 +0900

    Add docstrings
    See also: #11

--- iquip/cmdtools.py
+++ iquip/cmdtools.py
@@ -1,4 +1,15 @@
+"""Utility module for handling the command line."""
+
 import subprocess
 
 def run_command(cmd):
+    """Runs the command line and returns the result.
+
+    If the command line is completed without any error,
+    the printed result would be stored in the 'stdout' field of the return value.
+    Otherwise, the printed error would be stored in the 'stderr' field.
+    
+    Args:
+        cmd: The string of the command line to run.
+    """
     return subprocess.run(cmd, shell=True, capture_output=True, text=True)
