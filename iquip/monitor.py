23a61d154c828a97c579601488669491ba0a4951
Jiyong Kang <kangz12345@gmail.com>
2023-05-21 00:07:03 +0900

    Add `Monitor` class
    Generic type is used to annotate the various monitor objects.
    
    See also: #15

--- iquip/monitor.py
+++ iquip/monitor.py
@@ -1,1 +1,25 @@
 """Module for monitoring devices."""
+
+from typing import Any, TypeVar, Generic, Optional, Callable
+
+
+T = TypeVar("T")
+
+
+class Monitor(Generic[T]):
+    """An interface for a monitor.
+    
+    Monitors watch a single value, and notify any change of the value by calling
+      a callback function.
+    """
+
+    def __init__(self, updated_callback: Optional[Callable[[T], Any]] = None):
+        """
+        Args:
+            updated_callback: A function which will be called when the value is updated.
+        """
+        self.updated_callback = updated_callback
+
+    def value(self) -> Optional[T]:
+        """Returns the latest value or None if it is unknown or invalid."""
+        return None
