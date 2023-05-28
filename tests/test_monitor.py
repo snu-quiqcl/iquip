62fae63f126340ed1c4ab92621b4a1ffd73100d4
Jiyong Kang <kangz12345@gmail.com>
2023-05-29 01:49:18 +0900

    Add `test_initial_value()`

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,1 +1,17 @@
 """Unit tests for monitor module."""
+
+import unittest
+
+from iquip import monitor
+
+class TestMonitor(unittest.TestCase):
+    """Unit tests for Monitor class."""
+
+    def test_initial_value(self):
+        for value in (True, None, 1.0, "value", object()):
+            mon = monitor.Monitor(initial_value=value)
+            self.assertEqual(mon._value, value)
+
+
+if __name__ == "__main__":
+    unittest.main()
