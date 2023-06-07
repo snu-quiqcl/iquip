7ee036e6aeb719695edde2c0abe804aabd19a82a
BECATRUE <becatrueyjh@gmail.com>
2023-06-07 15:45:33 +0900

    Set-up a QApplication instance
    See also: #29

--- tests/test_explorer.py
+++ tests/test_explorer.py
@@ -3,5 +3,20 @@
 import unittest
 from unittest import mock
 
+from PyQt5.QtWidgets import QApplication
+
+import qiwis
+from iquip.apps import explorer
+
+class ExplorerAppTest(unittest.TestCase):
+    """Unit tests for ExplorerApp class."""
+
+    def setUp(self):
+        self.qapp = QApplication([])
+
+    def tearDown(self):
+        del self.qapp
+
+
 if __name__ == "__main__":
     unittest.main()
