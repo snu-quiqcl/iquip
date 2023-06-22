427a93e71cd9d7b9a9a1ce10d9907fd7e232097b
BECATRUE <becatrueyjh@gmail.com>
2023-06-22 13:30:27 +0900

    Initialize the builder app
    See also: #42

---
+++ iquip/apps/builder.py
@@ -0,0 +1,6 @@
+"""App module for editting the build arguments and submitting the experiment."""
+
+import qiwis
+
+class BuilderApp(qiwis.BaseApp):
+    """App for editting the build arguments and submitting the experiment."""
