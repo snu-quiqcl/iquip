e558144fc347b9ed4564f544ce534ec4171b238e
BECATRUE <becatrueyjh@gmail.com>
2023-07-03 09:35:41 +0900

    Create CodeViewerFrame class
    See also: #52

--- iquip/apps/visualizer.py
+++ iquip/apps/visualizer.py
@@ -1,10 +1,21 @@
 """App module for visualizing the experiment code."""
 
-from typing import Optional
+from typing import Optional, Tuple
 
 from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QVBoxLayout, QWidget
 
 import qiwis
+
+class CodeViewerFrame(QWidget):
+    """Frame for showing the code viewer."""
+
+    def __init__(self, parent: Optional[QWidget] = None):
+        """Extended."""
+        super().__init__(parent=parent)
+        # layout
+        layout = QVBoxLayout(self)
+
 
 class VisualizerApp(qiwis.BaseApp):
     """App for showing the code viewer and the sequence viewer."""
@@ -12,3 +23,8 @@ class VisualizerApp(qiwis.BaseApp):
     def __init__(self, name: str, parent: Optional[QObject] = None):
         """Extended."""
         super().__init__(name, parent=parent)
+        self.codeViewerFrame = CodeViewerFrame()
+
+    def frames(self) -> Tuple[CodeViewerFrame]:
+        """Overridden."""
+        return (self.codeViewerFrame,)
