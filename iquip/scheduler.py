d710a910a185322745893a5dd46ed0c625cc2d2f
giwon2004 <giwons04@naver.com>
2023-07-12 23:07:31 +0900

    Revert "Move scheduler to `explorer.py`"
    This reverts commit f6e56c843cb7b8bd8c723082bd6deaba26139e68.

---
+++ iquip/scheduler.py
@@ -0,0 +1,12 @@
+"""Protocol module for defining common forms."""
+
+from PyQt5.QtWidgets import QWidget, QLabel, QHBoxLayout
+from PyQt5.QtCore import pyqtSignal
+
+class SchedulerFrame(QWidget):
+    def __init__(self, experiments):
+        pass
+
+class ExperimentView(QWidget):
+    def __init__(self, jobid, etc):
+        pass
