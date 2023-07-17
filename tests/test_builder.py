d6383dcf45109bdcb8534760557fe3c5ce7b30f4
BECATRUE <becatrueyjh@gmail.com>
2023-07-17 10:21:08 +0900

    Create test file for builder module
    See also: #68

---
+++ tests/test_builder.py
@@ -0,0 +1,37 @@
+"""Unit tests for builder module."""
+
+import unittest
+from unittest import mock
+
+from iquip.apps import builder
+
+class _BaseEntryTest(unittest.TestCase):
+    """Unit tests for _BaseEntry class."""
+
+
+class _BooleanEntryTest(unittest.TestCase):
+    """Unit tests for _BooleanEntry class."""
+
+
+class _EnumerationEntryTest(unittest.TestCase):
+    """Unit tests for _EnumerationEntry class."""
+
+
+class _NumberEntryTest(unittest.TestCase):
+    """Unit tests for _NumberEntry class."""
+
+
+class _StringEntryTest(unittest.TestCase):
+    """Unit tests for _StringEntry class."""
+
+
+class _DateTimeEntryTest(unittest.TestCase):
+    """Unit tests for _DateTimeEntry class."""
+
+
+class ExperimentSubmitThreadTest(unittest.TestCase):
+    """Unit tests for ExperimentSubmitThread class."""
+
+
+class BuilderAppTest(unittest.TestCase):
+    """Unit tests for BuilderApp class."""
