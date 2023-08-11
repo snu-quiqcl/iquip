90aeff0645180e4f968cd89049f8e2cd63cf2475
BECATRUE <becatrueyjh@gmail.com>
2023-08-12 04:43:30 +0900

    Test typical scale of _NumberEntry
    See also: #87

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -152,6 +152,22 @@ class NumberEntryFunctionalTest(unittest.TestCase):
             entry = builder._NumberEntry(argName, argInfo)
             self.assertEqual(entry.value(), value)
 
+    def test_typical_scale(self):
+        """Tests when the scale for the unit is not typical."""
+        argInfo1 = {"unit": "us", "scale": 1e-6, "step": 1e-7, "min": 10 * 1e-6, "max": 100 * 1e-6,
+                    "ndecimals": 3, "type": "float", "default": 20 * 1e-6}
+        argInfo2 = {"unit": "us", "scale": 1e-7, "step": 1e-7, "min": 10 * 1e-6, "max": 100 * 1e-6,
+                    "ndecimals": 3, "type": "float", "default": 20 * 1e-6}
+        with mock.patch("iquip.apps.builder.compute_scale") as mocked_compute_scale:
+            mocked_compute_scale.return_value = 1e-6
+            for argName, argInfo, is_typical in (
+                ("name1", argInfo1, True),
+                ("name2", argInfo2, False)
+            ):
+                entry = builder._NumberEntry(argName, argInfo)
+                warningLabelText = "" if is_typical else "Not a typical scale for the unit."
+                self.assertEqual(entry.warningLabel.text(), warningLabelText)
+
 
 class DateTimeEntryFunctionalTest(unittest.TestCase):
     """Functional tests for _DateTimeEntry class."""
