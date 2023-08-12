68e91648cdb190a76e8dc093d19fd5b25ce6cd3c
giwon2004 <giwons04@naver.com>
2023-08-13 07:03:26 +0900

    Update unit test for `SubmittedExperimentInfo`

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -7,7 +7,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import Qt, QObject, QMimeData
 
 from iquip.apps import scheduler
-from iquip.protocols import ExperimentInfo
+from iquip.protocols import SubmittedExperimentInfo
 
 class ExperimentModelTest(unittest.TestCase):
     """Unit tests for ExperimentModel class."""
@@ -19,16 +19,16 @@ class ExperimentModelTest(unittest.TestCase):
         del self.qapp
 
     def test_row_count(self):
-        data1 = tuple(ExperimentInfo(str(i), {"rid": i, "priority": i}) for i in range(10))
-        data2 = tuple(ExperimentInfo(str(i), {"rid": i, "priority": 0}) for i in range(10))
+        data1 = tuple(SubmittedExperimentInfo(rid = i, priority = i) for i in range(10))
+        data2 = tuple(SubmittedExperimentInfo(rid = i, priority = 0) for i in range(10))
         for data in (data1, data2):
             model = scheduler.ExperimentModel()
             model.experimentQueue.extend(data)
             self.assertEqual(model.rowCount(), len(data))
 
     def test_data(self):
-        data1 = tuple(ExperimentInfo(str(i), {"rid": i, "priority": i}) for i in range(10))
-        data2 = tuple(ExperimentInfo(str(i), {"rid": i, "priority": 0}) for i in range(10))
+        data1 = tuple(SubmittedExperimentInfo(rid = i, priority = i) for i in range(10))
+        data2 = tuple(SubmittedExperimentInfo(rid = i, priority = 0) for i in range(10))
         for data in (data1, data2):
             model = scheduler.ExperimentModel()
             model.experimentQueue.extend(data)
@@ -38,9 +38,9 @@ class ExperimentModelTest(unittest.TestCase):
     def test_drop_mime_data(self):
         model = scheduler.ExperimentModel()
         data = (
-            ExperimentInfo("1", {"rid": 1, "priority": 2}),
-            ExperimentInfo("2", {"rid": 2, "priority": 1}),
-            ExperimentInfo("3", {"rid": 3, "priority": 1})
+            SubmittedExperimentInfo(rid = 1, priority = 2),
+            SubmittedExperimentInfo(rid = 2, priority = 1),
+            SubmittedExperimentInfo(rid = 3, priority = 1)
         )
         model.experimentQueue.extend(data)
         mime0 = QMimeData()
@@ -71,7 +71,7 @@ class SchedulerAppTest(unittest.TestCase):
     def test_add_experiment(self):
         app = scheduler.SchedulerApp(name="name")
         with mock.patch.object(app.schedulerFrame.model, "experimentQueue") as mocked_queue:
-            data = tuple(ExperimentInfo(str(i), {"rid": i, "priority": 10 - i}) for i in range(10))
+            data = tuple(SubmittedExperimentInfo(rid = i, priority = 10 - i) for i in range(10))
             for info in data:
                 app.addExperiment(info)
                 mocked_queue.append.assert_called_with(info)
@@ -80,7 +80,7 @@ class SchedulerAppTest(unittest.TestCase):
     def test_run_experiment(self):
         app = scheduler.SchedulerApp(name="name")
         with mock.patch.object(app.schedulerFrame, "runningView") as mocked_view:
-            info = ExperimentInfo("1", {"rid": 1, "priority": 1})
+            info = SubmittedExperimentInfo(rid = 1, priority = 1)
             app.runExperiment(info)
             mocked_view.updateInfo.assert_called_with(info)
 
@@ -99,7 +99,7 @@ class SchedulerFunctionalTest(unittest.TestCase):
         priorities = [1, 9, 3, 8, 7, 4, 2, 6, 5, 0]
         sorted_indices = [1, 3, 4, 7, 8, 5, 2, 6, 0, 9]
         data = tuple(
-            ExperimentInfo(str(i), {"rid": i, "priority": priorities[i]}) for i in range(10)
+            SubmittedExperimentInfo(rid = i, priority = priorities[i]) for i in range(10)
         )
         for info in data:
             app.addExperiment(info)
@@ -108,8 +108,8 @@ class SchedulerFunctionalTest(unittest.TestCase):
 
     def test_run_experiment(self):
         app = scheduler.SchedulerApp(name="name", parent=QObject())
-        experiment_run = ExperimentInfo("1", {"rid": 1, "priority": 1})
-        experiment_queue = ExperimentInfo("2", {"rid": 2, "priority": 2})
+        experiment_run = SubmittedExperimentInfo(rid = 1, priority = 1)
+        experiment_queue = SubmittedExperimentInfo(rid = 2, priority = 2)
         app.schedulerFrame.model.experimentQueue.append(experiment_run)
         app.schedulerFrame.model.experimentQueue.append(experiment_queue)
         app.runExperiment(experiment_run)
@@ -120,16 +120,16 @@ class SchedulerFunctionalTest(unittest.TestCase):
 
     def test_modify_experiment(self):
         app = scheduler.SchedulerApp(name="name", parent=QObject())
-        experiment_change = ExperimentInfo("1", {"rid": 1, "priority": 1})
-        experiment_new_info = ExperimentInfo("1", {"rid": 1, "priority": 2})
-        experiment_delete = ExperimentInfo("2", {"rid": 2, "priority": 1})
+        experiment_change = SubmittedExperimentInfo(rid = 1, priority = 1)
+        experiment_new_info = SubmittedExperimentInfo(rid = 1, priority = 2)
+        experiment_delete = SubmittedExperimentInfo(rid = 2, priority = 1)
         app.schedulerFrame.model.experimentQueue.append(experiment_delete)
         app.schedulerFrame.model.experimentQueue.append(experiment_change)
         app.changeExperiment(1, experiment_new_info)
-        self.assertEqual(app.schedulerFrame.model.experimentQueue[0].arginfo["priority"], 2)
-        self.assertEqual(app.schedulerFrame.model.experimentQueue[1].arginfo["rid"], 2)
+        self.assertEqual(app.schedulerFrame.model.experimentQueue[0].priority, 2)
+        self.assertEqual(app.schedulerFrame.model.experimentQueue[1].rid, 2)
         app.changeExperiment(1, None)
-        self.assertEqual(app.schedulerFrame.model.experimentQueue[0].arginfo["rid"], 1)
+        self.assertEqual(app.schedulerFrame.model.experimentQueue[0].rid, 1)
         self.assertEqual(app.schedulerFrame.model.rowCount(), 1)
 
 
