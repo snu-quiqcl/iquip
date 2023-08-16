44723b72eab86e673f0d2961cfec542aa5e267bf
Aijuh <aijuh1229@snu.ac.kr>
2023-08-16 17:52:27 +0900

    Update unittest for logger.py
    I updated unittest for logger.py.
    
    1. Add logFIlePath as argument for app.
    
    2. Specify handler at setLevel.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -20,10 +20,10 @@ class LoggingHandlerTest(unittest.TestCase):
 
     def test_emit(self):
         with mock.patch("iquip.apps.logger._Signaller.signal") as mocked_signal:
-            app = logger.LoggerApp(name="name", parent=QObject())
-            app.setLevel("DEBUG")
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
+            app.setLevel(app.frameHandler ,"DEBUG")
             test_log = logging.makeLogRecord({"name": "hello"})
-            app.handler.emit(test_log)
+            app.frameHandler.emit(test_log)
             mocked_signal.emit.assert_called_once()
 
 
@@ -38,13 +38,13 @@ class ConfirmClearingFrameTest(unittest.TestCase):
 
     def test_button_ok_clicked(self):
         with mock.patch("iquip.apps.logger.QWidget.close") as mocked_close:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.confirmFrame.buttonOKClicked()
             mocked_close.assert_called_once()
 
     def test_button_cancel_clicked(self):
         with mock.patch("iquip.apps.logger.QWidget.close") as mocked_close:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.confirmFrame.buttonCancelClicked()
             mocked_close.assert_called_once()
 
@@ -58,6 +58,11 @@ class LoggerAppTest(unittest.TestCase):
     def tearDown(self):
         del self.qapp
 
+    def test_init_logger(self):
+        app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
+        self.assertEqual(app.frameHandler.level, logging.WARNING)
+        self.assertEqual(app.fileHandler.level, logging.WARNING)
+
     def test_set_level(self):
         levels = {
             "DEBUG": logging.DEBUG,
@@ -67,51 +72,53 @@ class LoggerAppTest(unittest.TestCase):
             "CRITICAL": logging.CRITICAL
         }
         root_logger = logging.getLogger()
-        app = logger.LoggerApp(name="name", parent=QObject())
+        app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
         for levelText, level in levels.items():
-            app.setLevel(levelText)
-            self.assertEqual(app.handler.level, level)
+            app.setLevel(app.frameHandler, levelText)
+            app.setLevel(app.fileHandler, levelText)
+            self.assertEqual(app.frameHandler.level, level)
             self.assertEqual(root_logger.level, level)
-        prev_level = app.handler.level
+        prev_level = app.frameHandler.level
         non_level = "non_level"
-        app.setLevel(non_level)
-        self.assertEqual(prev_level, app.handler.level)
+        app.setLevel(app.frameHandler, non_level)
+        app.setLevel(app.fileHandler, non_level)
+        self.assertEqual(prev_level, app.frameHandler.level)
         self.assertEqual(prev_level, root_logger.level)
 
     def test_frames(self):
-        app = logger.LoggerApp(name="name", parent=QObject())
+        app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
         self.assertEqual(app.frames(), (app.loggerFrame,))
 
     def test_call_check_to_clear(self):
         with mock.patch("iquip.apps.logger.LoggerApp.checkToClear") as mocked_method:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.loggerFrame.clearButton.clicked.emit()
             mocked_method.assert_called_once()
 
     def test_check_to_clear(self):
         with mock.patch("iquip.apps.logger.QWidget.show") as mocked_show:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.checkToClear()
             mocked_show.assert_called_once()
 
     def test_call_clear_log(self):
         with mock.patch("iquip.apps.logger.LoggerApp.clearLog") as mocked_method:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.confirmFrame.buttonBox.accepted.emit()
             mocked_method.assert_called_once()
 
     @mock.patch("iquip.apps.logger.QTextEdit.clear")
     def test_clear_log(self, mocked_clear):
         with mock.patch("iquip.apps.logger.logger") as mocked_logger:
-            app = logger.LoggerApp(name="name", parent=QObject())
+            app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
             app.clearLog()
             mocked_clear.assert_called_once()
             mocked_logger.info.called_once_with("Tried to clear logs by clicking clear button")
 
     def test_add_log(self):
+        app = logger.LoggerApp(name="name", logFilePath = "", parent=QObject())
         with mock.patch("iquip.apps.logger.QTextEdit.insertPlainText") as mocked_insert:
             with mock.patch("iquip.apps.logger.QDateTime.currentDateTime") as mocked_time:
-                app = logger.LoggerApp(name="name", parent=QObject())
                 app.addLog("hello")
                 timeString = mocked_time().toString("yyyy-MM-dd HH:mm:ss")
                 msg = f"{timeString}: hello\n"
