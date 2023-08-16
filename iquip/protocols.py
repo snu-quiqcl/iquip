3a53cfca65371e2a98cea541329da1044e9189cd
giwon2004 <giwons04@naver.com>
2023-08-16 14:52:57 +0900

    resolve conflicts manually

--- iquip/protocols.py
+++ iquip/protocols.py
@@ -37,6 +37,6 @@ class SubmittedExperimentInfo:
     expid: Dict[str, Any] = dataclasses.field(default_factory = dict)
     due_date: str = ""
 
-    def items(self) -> Any:
+    def items(self) -> ItemsView[str, Any]:
         """Returns the attributes of the experiment."""
         return self.__dict__.items()
