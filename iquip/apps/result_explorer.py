1e47ff81486aa306309110b57789dccc8c4c77db
BECATRUE <becatrueyjh@gmail.com>
2023-09-01 16:45:10 +0900

    Create `ResultExplorerFrame`
    See also: #93

--- iquip/apps/result_explorer.py
+++ iquip/apps/result_explorer.py
@@ -1,6 +1,33 @@
-"""App module for showing the RID list and opening a result visualizer."""
+"""App module for showing the simple results and opening a result visualizer."""
+
+from typing import Optional
+
+from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem, QPushButton, QVBoxLayout, QWidget
 
 import qiwis
 
+class ResultExplorerFrame(QWidget):
+    """Frame for showing the RID list and a specific h5 result."""
+
+    def __init__(self, parent: Optional[QWidget] = None):
+        """Extended."""
+        super().__init__(parent=parent)
+        # widgets
+        self.ridList = QListWidget(self)
+        self.resultList = QListWidget(self)
+        self.openButton = QPushButton("Visualize", self)
+        # layout
+        layout = QVBoxLayout
+        layout.addWidget(self.ridList)
+        layout.addWidget(self.resultList)
+        layout.addWidget(self.openButton)
+        self.setLayout(layout)
+        
+
 class ResultExplorerApp(qiwis.BaseApp):
-    """App for showing the RID list and a detailed result."""
+    """App for showing the RID list and a specific h5 result."""
+
+    def __init__(self, name: str, parent: Optional[QObject] = None):
+        """Extended."""
+        super().__init__(name, parent=parent)
