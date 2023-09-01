4b00e9c90e2dd5f13be2648957f887d1df44b796
BECATRUE <becatrueyjh@gmail.com>
2023-09-01 15:48:38 +0900

    Create `result_explorer.py`
    See also: #93

---
+++ iquip/apps/result_explorer.py
@@ -0,0 +1,6 @@
+"""App module for showing the RID list and opening a result visualizer."""
+
+import qiwis
+
+class ResultExplorerApp(qiwis.BaseApp):
+    """App for showing the RID list and a detailed result."""
