88ec5589b484f3303d0b80d7e8881e77eaec212e
Jiyong Kang <kangz12345@gmail.com>
2023-09-12 13:39:43 +0900

    Add docstring to `AxisInfo`

--- iquip/apps/dataviewer.py
+++ iquip/apps/dataviewer.py
@@ -2,14 +2,30 @@
 
 import dataclasses
 import logging
-from typing import Iterable
+from typing import Iterable, Optional
 
 logger = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
 class AxisInfo:
-    """Axis information of NDArray data."""
+    """Axis information of ndarray data.
+
+    Usually each axis of an ndarray corresponds to a specific scan parameter.
+    For example, if one counts the photons for 100 shots and t in range(0, 10),
+      it yields 2 dimensional results (in total 1000 data points) whose shape
+      is (10, 100) - axis 0 is "t" and axis 1 is "shot".
+    
+    Fields:
+        name: The name describing the axis.
+        values: The parameter values for the axis. The length should be equal
+          to the corresponding ndarry size of the axis. If unit is given, the
+          values should be in that unit, without any unit prefix, e.g., in Hz,
+          not kHz. In some cases, this might not be numeric values but string,
+          etc.
+        unit: The unit of the values without any unit prefix, e.g., u, m, k, M.
+    """
     name: str
-    unit: str
     values: Iterable
+    unit: Optional[str] = None
+
