ab46da0ba5e695c81db38b60d4ee1a1d30929150
BECATRUE <becatrueyjh@gmail.com>
2023-09-20 13:20:05 +0900

    Define `TTLControllerWidget` in "apps/monitor.py"
    See also: #153

---
+++ iquip/apps/monitor.py
@@ -0,0 +1,6 @@
+"""App module for monitoring and controlling ARTIQ hardwares e.g., TTL, DDS, and DAC."""
+
+from PyQt5.QtWidgets import QWidget
+
+class TTLControllerWidget(QWidget):
+    """Single TTL channel controller widget."""
