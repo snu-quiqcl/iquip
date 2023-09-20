e781b0e455d30f74fabd3f2627fd8306ae9ff5e5
BECATRUE <becatrueyjh@gmail.com>
2023-09-20 14:13:01 +0900

    Add name and channel labels
    See also: #153

--- iquip/apps/monitor.py
+++ iquip/apps/monitor.py
@@ -1,6 +1,26 @@
 """App module for monitoring and controlling ARTIQ hardwares e.g., TTL, DDS, and DAC."""
 
-from PyQt5.QtWidgets import QWidget
+from typing import Optional
+
+from PyQt5.QtWidgets import QHBoxLayout, QLabel, QVBoxLayout, QWidget
 
 class TTLControllerWidget(QWidget):
     """Single TTL channel controller widget."""
+
+    def __init__(self, name: str, channel: int, parent: Optional[QWidget] = None):
+        """Extended.
+        
+        Args:
+            name: TTL channel name.
+            channel: TTL channel number.
+        """
+        super().__init__(parent=parent)
+        # widgets
+        nameLabel = QLabel(name, self)
+        channelLabel = QLabel(f"CH {channel}", self)
+        # layout
+        infoLayout = QHBoxLayout()
+        infoLayout.addWidget(nameLabel)
+        infoLayout.addWidget(channelLabel)
+        layout = QVBoxLayout(self)
+        layout.addLayout(infoLayout)
