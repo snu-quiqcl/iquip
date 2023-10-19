137bc0e77a66b935a420bffac25f5bcb85989f68
Jiyong Kang <kangz12345@gmail.com>
2023-10-20 01:54:11 +0900

    Connect thread stopped and finished signals

--- iquip/apps/dataviewer.py
+++ iquip/apps/dataviewer.py
@@ -952,6 +952,9 @@ class DataViewerApp(qiwis.BaseApp):
             self.constants.proxy_port,  # pylint: disable=no-member
         )
         self.thread.initialized.connect(self.setDataset, type=Qt.QueuedConnection)
+        self.thread.stopped.connect(realtimePart.setStatus)
+        self.thread.finished.connect(functools.partial(realtimePart.setStatus, sync=False))
+        self.thread.finished.connect(self.thread.deleteLater)
         self.thread.start()
 
     @pyqtSlot(np.ndarray, list, list)
