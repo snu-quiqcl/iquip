620de81d9e3e524ff80d654afe8073254e9e12d3
Jung Eui Hwan <43592775+Aijuh@users.noreply.github.com>
2024-01-03 17:31:07 +0900

    RangeScan

--- iquip/apps/builder.py
+++ iquip/apps/builder.py
@@ -2,13 +2,16 @@
 
 import json
 import logging
+from collections import OrderedDict
+import random
 from typing import Any, Dict, Optional, Tuple, Union
 
 import requests
 from PyQt5.QtCore import QDateTime, QObject, Qt, QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import (
-    QCheckBox, QComboBox, QDateTimeEdit, QDoubleSpinBox, QHBoxLayout, QLabel, QLineEdit,
-    QListWidget, QListWidgetItem, QPushButton, QVBoxLayout, QWidget
+    QCheckBox, QComboBox, QDateTimeEdit, QDoubleSpinBox, QSpinBox, QLabel, QLineEdit,
+    QListWidget, QListWidgetItem, QPushButton, QVBoxLayout, QHBoxLayout, QGridLayout,
+    QWidget, QStackedWidget, QRadioButton, QButtonGroup
 )
 
 import qiwis
@@ -297,10 +300,14 @@ class BuilderFrame(QWidget):
     Attributes:
         experimentNameLabel: The label for showing the experiment name.
         experimentClsNameLabel: The label for showing the class name of the experiment.
+        argsStackWidget: The stack widget that contains two list widget.
         argsListWidget: The list widget with the build arguments.
+        scanListWidget: The list widget with the scannable arguments.
         reloadArgsButton: The button for reloading the build arguments.
         schedOptsListWidget: The list widget with the schedule options.
         submitButton: The button for submitting the experiment.
+        radioButtons: The button dictionary that has scannable or NonScan buttons.
+        argType: The button group for selecting whether scannable or NonScan.
     """
 
     def __init__(
@@ -319,15 +326,28 @@ class BuilderFrame(QWidget):
         # widgets
         self.experimentNameLabel = QLabel(f"Name: {experimentName}", self)
         self.experimentClsNameLabel = QLabel(f"Class: {experimentClsName}", self)
+        self.argsStackWidget = QStackedWidget(self)
         self.argsListWidget = QListWidget(self)
+        self.scanListWidget = QListWidget(self)
         self.reloadArgsButton = QPushButton("Reload", self)
         self.schedOptsListWidget = QListWidget(self)
         self.submitButton = QPushButton("Submit", self)
+        self.radioButtons = OrderedDict()
+        self.radioButtons["NonScan"] = QRadioButton("NonScan")
+        self.radioButtons["Scannable"] = QRadioButton("Scannable")
+        self.argType = QButtonGroup(self)
         # layout
+        self.argsStackWidget.addWidget(self.argsListWidget)
+        self.argsStackWidget.addWidget(self.scanListWidget)
         layout = QVBoxLayout(self)
+        buttonLayout = QGridLayout()
         layout.addWidget(self.experimentNameLabel)
         layout.addWidget(self.experimentClsNameLabel)
-        layout.addWidget(self.argsListWidget)
+        for n, b in enumerate(self.radioButtons.values()):
+            buttonLayout.addWidget(b, 0, n)
+            self.argType.addButton(b)
+        layout.addLayout(buttonLayout)
+        layout.addWidget(self.argsStackWidget)
         layout.addWidget(self.reloadArgsButton)
         layout.addWidget(self.schedOptsListWidget)
         layout.addWidget(self.submitButton)
@@ -449,6 +469,9 @@ class BuilderApp(qiwis.BaseApp):
         # connect signals to slots
         self.builderFrame.reloadArgsButton.clicked.connect(self.reloadArgs)
         self.builderFrame.submitButton.clicked.connect(self.submit)
+        for button in self.builderFrame.radioButtons.values():
+            button.toggled.connect(self.buttonToggled)
+        self.builderFrame.radioButtons["NonScan"].setChecked(True)
 
     def initArgsEntry(self, experimentInfo: ExperimentInfo):
         """Initializes the build arguments entry.
@@ -456,20 +479,33 @@ class BuilderApp(qiwis.BaseApp):
         Args:
             experimentInfo: The experiment information.
         """
+        nonScanEntry = ["BooleanValue", "StringValue", "EnumerationValue", "NumberValue"]
         for argName, (argInfo, *_) in experimentInfo.arginfo.items():
             # TODO(BECATRUE): The other types such as 'Scannable'
             # will be implemented in Basic Runner project.
-            entryCls = {
-                "BooleanValue": _BooleanEntry,
-                "StringValue": _StringEntry,
-                "EnumerationValue": _EnumerationEntry,
-                "NumberValue": _NumberEntry
-            }[argInfo.pop("ty")]
-            widget = entryCls(argName, argInfo)
-            item = QListWidgetItem(self.builderFrame.argsListWidget)
-            item.setSizeHint(widget.sizeHint())
-            self.builderFrame.argsListWidget.addItem(item)
-            self.builderFrame.argsListWidget.setItemWidget(item, widget)
+            argType = argInfo.pop("ty")
+            if argType in nonScanEntry:
+                entryCls = {
+                    "BooleanValue": _BooleanEntry,
+                    "StringValue": _StringEntry,
+                    "EnumerationValue": _EnumerationEntry,
+                    "NumberValue": _NumberEntry,
+                }[argType]
+                widget = entryCls(argName, argInfo)
+                item = QListWidgetItem(self.builderFrame.argsListWidget)
+                item.setSizeHint(widget.sizeHint())
+                self.builderFrame.argsListWidget.addItem(item)
+                self.builderFrame.argsListWidget.setItemWidget(item, widget)
+            elif argType == "Scannable":
+                widget = _ScanEntry(argName, argInfo)
+                item = QListWidgetItem(self.builderFrame.scanListWidget)
+                item.setSizeHint(widget.sizeHint())
+                self.builderFrame.scanListWidget.addItem(item)
+                self.builderFrame.scanListWidget.setItemWidget(item, widget)
+            else:
+                # print format should be checked
+                logger.warning("Invalid argument type at experiment: %s, argument name: %s",
+                               experimentInfo["name"], argName)
 
     def initSchedOptsEntry(self):
         """Initializes the scheduler options entry.
@@ -584,6 +620,240 @@ class BuilderApp(qiwis.BaseApp):
         """
         logger.info("RID: %d", rid)
 
+    @pyqtSlot()
+    def buttonToggled(self):
+        """Selects scanListWidget or argsListWidget at argument layout.
+        
+        Once the selected button in radioButtons at BuilderFrame is changed, this is called.
+        """
+        frame = self.builderFrame
+        for ty, button in self.builderFrame.radioButtons.items():
+            if button.isChecked():
+                if ty == 'NonScan':
+                    self.builderFrame.argsStackWidget.setCurrentWidget(frame.argsListWidget)
+                else:
+                    self.builderFrame.argsStackWidget.setCurrentWidget(frame.scanListWidget)
+                break
+
     def frames(self) -> Tuple[Tuple[str, BuilderFrame]]:
         """Overridden."""
         return (("", self.builderFrame),)
+
+class _RangeScan(QWidget):
+    """An widget for a RangeScan type in Scannable Entry.
+
+    Attributes:
+        layout: The layout of _RangeScan widget.
+    """
+    def __init__(
+        self,
+        procdesc: Dict[str, Any],
+        state: Dict[str, Any],
+        parent: Optional[QWidget] = None
+        ):
+        """Extended.
+
+        Args:
+            procdesc: Each key and its value are:
+                unit: The unit of the number value.
+                scale: The scale factor that is multiplied to the number value.
+                global_step: The step between values changed by the up and down button.
+                global_min: The minimum value. (default=float("-inf"))
+                global_max: The maximum value. (default=float("inf"))
+                If min > max, then they are swapped.
+                ndecimals: The number of displayed decimals.
+            state:
+                start: Start point for range scan.
+                stop: End point for range scan.
+                npoints: The number of points to be genereated in given range.
+                randomize: Boolean value that decides whether to shuffle the order of scans.
+        """
+        super().__init__(parent=parent)
+        scale = procdesc["scale"]
+        def apply_properties(widget):
+            """Adds properties to the SpinBox widget.
+
+            Attributes:
+               widget: A QDOubleSpinWidget that has properties to set. 
+            """
+            widget.setDecimals(procdesc["ndecimals"])
+            if procdesc["global_min"] is not None:
+                widget.setMinimum(procdesc["global_min"]/scale)
+            else:
+                widget.setMinimum(float("-inf"))
+            if procdesc["global_max"] is not None:
+                widget.setMaximum(procdesc["global_max"]/scale)
+            else:
+                widget.setMaximum(float("inf"))
+            if procdesc["global_step"] is not None:
+                widget.setSingleStep(procdesc["global_step"]/scale)
+            if procdesc["unit"]:
+                widget.setSuffix(" " + procdesc["unit"])
+        self.layout = QGridLayout(self)
+        start = QDoubleSpinBox(self)
+        apply_properties(start)
+        start.setValue(state["start"]/scale)
+        npoints = QSpinBox(self)
+        npoints.setMinimum(1)
+        npoints.setMaximum((1 << 31) - 1)
+        npoints.setValue(state["npoints"])
+        stop = QDoubleSpinBox(self)
+        apply_properties(stop)
+        stop.setValue(state["stop"]/scale)
+        randomize = QCheckBox("Randomize", self)
+        randomize.setChecked(state["randomize"])
+        #layout
+        self.layout.addWidget(QLabel("start:", self), 0, 0)
+        self.layout.addWidget(start, 0, 1)
+        self.layout.addWidget(QLabel("npoints:", self), 1, 0)
+        self.layout.addWidget(npoints, 1, 1)
+        self.layout.addWidget(QLabel("stop:", self), 2, 0)
+        self.layout.addWidget(stop, 2, 1)
+        self.layout.addWidget(randomize, 3, 1)
+
+        def update_start(value):
+            """Updates the current start value at _RangeScan argument.
+            
+            Once the value at start SpinBox changed, this is called.
+            """
+            state["start"] = value*scale
+            if start.value() != value:
+                start.setValue(value)
+
+        def update_stop(value):
+            """Updates the current stop value at _RangeScan argument.
+            
+            Once the value at stop SpinBox changed, this is called.
+            """
+            state["stop"] = value*scale
+            if stop.value() != value:
+                stop.setValue(value)
+
+        def update_npoints(value):
+            """Updates the current npoints number at _RangeScan argument.
+            
+            Once the number at npoints SpinBox changed, this is called.
+            """
+            state["npoints"] = value
+            if npoints.value() != value:
+                npoints.setValue(value)
+
+        def update_randomize():
+            """Updates the current randomize boolean value at _RangeScan argument.
+            
+            Once the checked state of randomize button changed, this is called.
+            """
+            state["randomize"] = randomize.isChecked()
+            randomize.setChecked(state["randomize"])
+
+        start.valueChanged.connect(update_start)
+        npoints.valueChanged.connect(update_npoints)
+        stop.valueChanged.connect(update_stop)
+        randomize.stateChanged.connect(update_randomize)
+
+class _ScanEntry(QWidget):
+    """Entry class for a Scannable argument.
+    
+    Attributes:
+        name: The name of the experiment.
+        state: The Dictionary that describes arguments of each scan type.
+        stack: The stackWidget that contains widget of each scan type.
+        widgets: The Dictionary that contains widget of each scan type.
+        radioButtons: The Dictionary that contains buttons for selecting specific scan type.
+        layout: The layout of _ScanEntry widget.
+    """
+    def __init__(
+        self,
+        name: str,
+        argInfo: Dict[str, Any],
+        parent: Optional[QWidget] = None
+        ):
+        """Extended.
+
+        Args:
+            name: The name of the experiment.
+            argInfo: Each key and its value are:
+                default: The Dictionary that describes arguments of a specific scanning type.
+                unit: The unit of the number value.
+                scale: The scale factor that is multiplied to the number value.
+                global_step: The step between values changed by the up and down button.
+                global_min: The minimum value. (default=float("-inf"))
+                global_max: The maximum value. (default=float("inf"))
+                If min > max, then they are swapped.
+                ndecimals: The number of displayed decimals.
+        """
+        super().__init__(parent=parent)
+        self.name = name
+        procdesc = self.get_procdesc(argInfo)
+        self.state = self.get_state(argInfo)
+        self.widget = _RangeScan(procdesc, self.state["RangeScan"])
+        self.layout = QGridLayout(self)
+        self.layout.addWidget(QLabel(name, self), 0, 0)
+        self.layout.addWidget(self.widget, 0, 1)
+
+    def get_state(self, argInfo):
+        """Gets state dictionary that describes scanning parameter.
+
+        Creates a default state dictionary and reflect the scan argInfo.
+
+        Args:
+            argInfo: The dictionary with the argument options.
+        """
+        scale = argInfo["scale"]
+        state = {
+            "selected": "RangeScan",
+            "RangeScan": {"start": 0.0, "stop": 100.0*scale, "npoints": 10,
+                          "randomize": False, "seed": None},
+        }
+        if "default" in argInfo:
+            defaults = argInfo["default"]
+            if not isinstance(defaults, list):
+                defaults = [defaults]
+            state["selected"] = defaults[0]["ty"]
+            for default in reversed(defaults):
+                ty = default["ty"]
+                if ty == "RangeScan":
+                    state[ty]["start"] = default["start"]
+                    state[ty]["stop"] = default["stop"]
+                    state[ty]["npoints"] = default["npoints"]
+                    state[ty]["randomize"] = default["randomize"]
+                    state[ty]["seed"] = default["seed"]
+                else:
+                    logger.warning("unknown scannable type: %s", ty)
+        return state
+
+    def get_procdesc(self, argInfo):
+        """Gets prodesc dictionary that describes global scanning parameter.
+
+        Args:
+            argInfo: The dictionary with the argument options.
+        """
+        procdesc = {
+            "unit": argInfo["unit"],
+            "scale": argInfo["scale"],
+            "global_step": argInfo["global_step"],
+            "ndecimals": argInfo["ndecimals"],
+            "global_min": argInfo["global_min"],
+            "global_max":  argInfo["global_max"]
+        }
+        return procdesc
+
+    def get_sequence(self):
+        """Gets scanning sequence from _ScanEntry."""
+        sequence = None
+        if self.state["selected"] == "RangeScan":
+            npoints = self.state["RangeScan"]["npoints"]
+            start = self.state["RangeScan"]["start"]
+            stop = self.state["RangeScan"]["stop"]
+            if npoints == 0:
+                sequence = []
+            if npoints == 1:
+                sequence = [self.start]
+            else:
+                dx = (stop - start)/(npoints - 1)
+                sequence = [i*dx + start for i in range(npoints)]
+            if self.state["RangeScan"]["randomize"]:
+                random.Random(self.state["RangeScan"]["seed"]).shuffle(sequence)
+        else:
+            logger.warning("unknown scannable type: %s", self.state["selected"] )
+        return sequence
