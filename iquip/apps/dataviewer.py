408b0fc00d81c9146389713ddddae320aa048e89
BECATRUE <becatrueyjh@gmail.com>
2024-01-12 12:36:41 +0900

    Connect to web socket and remove `_get()`
    See also: #236

--- iquip/apps/dataviewer.py
+++ iquip/apps/dataviewer.py
@@ -922,13 +922,7 @@ class _DatasetFetcherThread(QThread):
     modified = pyqtSignal(list)
     stopped = pyqtSignal(str)
 
-    def __init__(
-        self,
-        name: str,
-        ip: str,
-        port: int,
-        parent: Optional[QObject] = None,
-    ):
+    def __init__(self, name: str, ip: str, port: int, parent: Optional[QObject] = None):
         """Extended.
         
         Args:
@@ -942,31 +936,6 @@ class _DatasetFetcherThread(QThread):
         self.websocket: Optional[ClientConnection] = None
         self.mutex = QMutex()
         self.modifyDone = QWaitCondition()
-
-    def _get(
-        self,
-        path: str,
-        params: Dict[str, Any],
-        default: Any = None,
-        timeout: float = 10
-    ) -> Any:
-        """Returns the json()-ed response of a GET request.
-        
-        Args:
-            path: The API path, i.e., the request url is "http://{ip}:{port}/{path}".
-            params: Params argument for the GET request.
-            default: The return value of this method when an exception occurs
-              during the GET request.
-            timeout: Timeout argument for the GET request.
-        """
-        url = f"http://{self.ip}:{self.port}/{path}"
-        try:
-            response = requests.get(url, params=params, timeout=timeout)
-            response.raise_for_status()
-        except requests.exceptions.RequestException:
-            logger.exception("Failed to GET %s", url)
-            return default
-        return response.json()
 
     def _initialize(self) -> float:
         """Fetches the target dataset to initialize the local dataset.
@@ -999,6 +968,10 @@ class _DatasetFetcherThread(QThread):
 
     def run(self):
         """Overridden."""
+        try:
+            self.websocket = connect(self.url)
+        except WebSocketException:
+            logger.exception("Failed to fetch the dataset.")
         timestamp = self._initialize()
         if timestamp < 0:
             self.stopped.emit("Failed to get dataset.")
