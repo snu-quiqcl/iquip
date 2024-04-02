86f0ac25c01eb927530ff93e41bbd4a8e57495a8
BECATRUE <becatrueyjh@gmail.com>
2024-04-02 16:16:42 +0900

    Not use bare except

--- iquip/apps/scheduler.py
+++ iquip/apps/scheduler.py
@@ -78,7 +78,7 @@ class _ScheduleFetcherThread(QThread):
                             arguments=expid["arguments"]
                         ))
                     self.fetched.emit(schedule)
-        except:  # pylint: disable=bare-except
+        except Exception:
             logger.exception("Failed to fetch the schedule.")
 
 
