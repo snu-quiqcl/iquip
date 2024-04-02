8f16352b4fa354c1100e371b9185e917610cc6cb
Jiyong Kang <kangz12345@gmail.com>
2024-04-02 20:09:18 +0900

    Emit signals in `use_client` decorator

--- iquip/apps/stage.py
+++ iquip/apps/stage.py
@@ -34,17 +34,18 @@ def use_client(function: Callable[..., None]) -> Callable[..., None]:
         client = self._clients.get(key, None)  # pylint: disable=protected-access
         if client is None:
             logger.error("Failed to get client %s.", key)
+            self.clientError.emit(key, KeyError(f"There is no client {key}"))
             return
         try:
             function(self, client, *args, **kwargs)
-        except (AttributeError, OSError, ValueError):
+        except (AttributeError, OSError, ValueError) as error:
             logger.exception(
                 "Error occurred while running %s with client %s.",
                 function.__name__,
                 key,
             )
-            client.close_rpc()
-            self._clients.pop(key)  # pylint: disable=protected-access
+            self.clientError.emit(key, error)
+            self._closeTarget(key)
     return wrapped
 
 
