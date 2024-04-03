bcac940b97f54960e086f0b92b3e29f640ec0ec0
BECATRUE <becatrueyjh@gmail.com>
2024-04-03 17:18:20 +0900

    Fix a Pylint warning

--- iquip/apps/monitor.py
+++ iquip/apps/monitor.py
@@ -232,7 +232,7 @@ class _TTLStatusThread(QThread):
                         break  # connection is lost
                     status = json.loads(response)
                     self.fetched.emit(status)
-        except Exception:
+        except Exception:  # pylint: disable=broad-exception-caught
             logger.exception("Failed to fetch the modifications of TTL status.")
 
 
