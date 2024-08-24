6d7ee163179b711ab8bfda4c63a181c42bf0f414
ParkJeonghyun <49219392+alexist2623@users.noreply.github.com>
2024-08-24 15:57:15 +0900

    cpp, c statement added

--- iquip/apps/explorer.py
+++ iquip/apps/explorer.py
@@ -183,7 +183,11 @@ class ExplorerApp(qiwis.BaseApp):
                 experimentFileItem.setText(0, experimentFile[:-1])
                 # Make an empty item for indicating that it is a directory.
                 QTreeWidgetItem(experimentFileItem)
-            elif experimentFile.endswith(".py"):
+            elif (
+                experimentFile.endswith(".cpp") or 
+                experimentFile.endswith(".c") or 
+                experimentFile.endswith(".py")
+            ):
                 experimentFileItem = QTreeWidgetItem(widget)
                 experimentFileItem.setText(0, experimentFile)
 
